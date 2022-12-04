9c57fc27c368361f5170fcde6a3081613a67fc18
aleji0 <alex.springer@gmail.com>
2022-12-04 15:45:22 -0700

    middlewar

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const xprsHbrs = require('express-handlebars');
+const xphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3002;
 
 const sequelize = require('./config/config');
+const { Sequelize } = require('sequelize');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -20,5 +21,17 @@ const sess = {
     },
     resave: false,
     saveUninitialized: true,
-    store: new SequelizeStore
-}
+    store: new SequelizeStore({
+        db: sequelize
+    })
+},
+    app.use(session(sess));
+
+    const hdbrs = xphbs.create({ helpers });
+
+    app.engine(express.json());
+    app.set('view engine', 'handlebars');
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    
+
